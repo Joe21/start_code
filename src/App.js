bc87b569f90034272608676c498d3bf793b917b0
Joe21 <=>
2021-05-29 23:17:36 -0400

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <header>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
